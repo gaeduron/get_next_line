6cd0e855e0ae4c344a75bb199d3ecf1cb2fe0811
Gaetan DURON <gduron@e2r6p1.42.fr>
2017-04-14 20:06:01 +0200

    added .c

---
+++ get_next_line.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gduron <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/14 16:05:06 by gduron            #+#    #+#             */
+/*   Updated: 2017/04/14 20:05:28 by gduron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		get_next_line(const int fd, char **line)
+{
+	static char	*file_str;
+	char 		*to_return;
+	char		*to_keep;
+	
+
+	to_
+
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		dest = read_loop(dest, buff, ret, &test);
+		if (dest == 0)
+		{
+			free(buff);
+			return (0);
+		}
+		i++;
+	}
+}
