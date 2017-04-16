c4e0f3ddab4ccf8f09906ce01dc056684da23269
gaetan duron <gaetan.duron@gmail.com>
2017-04-16 23:27:09 +0200

    added some code

--- get_next_line.c
+++ get_next_line.c
@@ -6,29 +6,60 @@
 /*   By: gduron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 16:05:06 by gduron            #+#    #+#             */
-/*   Updated: 2017/04/14 20:05:28 by gduron           ###   ########.fr       */
+/*   Updated: 2017/04/16 23:26:25 by bduron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int 	find_next_newline(t_gnl *gnl)
+{
+	int		i;
+
+	i = 0;
+	while (gnl->str[gnl->index + i] && gnl->str[gnl->index + i] == '\n')
+		i++;
+}
+
+int		waiting_line(t_gnl *gnl, char **line)
+{
+	if (!find_next_newline(gnl))
+		return(0);
+	
+	*line = next_line(gnl->str);
+	return (1)
+}
+
+
+
+
+
+
+
 int		get_next_line(const int fd, char **line)
 {
-	static char	*file_str;
-	char 		*to_return;
-	char		*to_keep;
-	
+	static t_gnl	*gnl;
 
-	to_
-
+	if (fd < 0 || line == 0 || *line == 0 || BUFF_SIZE < 1)
+		return (-1);
+	if (gnl == 0)
+		init_gnl(gnl);
+	if (waiting_line(gnl, line))
+		return (1);
+    if (!(buff = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+        return (0);
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		dest = read_loop(dest, buff, ret, &test);
-		if (dest == 0)
+		gnl.str = read_loop(gnl.str, buff, ret);
+		if (gnl.str == 0)
 		{
 			free(buff);
-			return (0);
+			return (-1);
 		}
-		i++;
+		
 	}
+	free(gnl->str);
+	free(buff);
+	return (ret == 0 ? 0 : -1);
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,18 @@
 /*   By: gduron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 15:57:40 by gduron            #+#    #+#             */
-/*   Updated: 2017/04/14 16:02:51 by gduron           ###   ########.fr       */
+/*   Updated: 2017/04/16 20:09:14 by bduron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-int		get_next_line(const int fd, char **line);
+int				get_next_line(const int fd, char **line);
+
+typedef struct	s_gnl
+{
+	char		*str;
+}				t_gnl;
 # define BUFF_SIZE 32
 #endif
