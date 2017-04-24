2b370864efd5d80aff97420cd8f0117c0f5786a5
Gaetan DURON <gduron@e3r7p23.42.fr>
2017-04-24 19:40:18 +0200

    all basic test on 42fc

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gduron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 16:05:06 by gduron            #+#    #+#             */
-/*   Updated: 2017/04/24 19:12:29 by gduron           ###   ########.fr       */
+/*   Updated: 2017/04/24 19:36:38 by gduron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,15 @@ int		waiting_line(t_gnl *g, char **line, char *nl)
 	nl = ft_strchr(g->s, '\n');
 	nl ? *line = ft_strsub(g->s, 0, nl - g->s) : (*line = ft_strdup(g->s));
 	nl ? (g->s = ft_memmove(g->s, nl + 1, ft_strlen(nl))) : 0;
+	if (g->s[0] == 0 && !nl)
+	{
+		!nl ? free(g->s) : 0;
+		!nl ? g->s = 0 : 0;
+		return (0);
+	}
 	!nl ? free(g->s) : 0;
 	!nl ? g->s = 0 : 0;
-	return (nl ? 1 : 0);
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -41,7 +47,7 @@ int		get_next_line(const int fd, char **line)
 			!(buff = ft_strnew(BUFF_SIZE + 1)))
 		return (-1);
 	g == 0 ? g = init_gnl(g) : 0;
-	while ((ret = read(fd, buff, BUFF_SIZE)) > 0 || g->s)
+	while (((ret = read(fd, buff, BUFF_SIZE)) > 0 || g->s) && ret != -1)
 	{
 		buff[ret] = '\0';
 		!(buff = ft_strjoin(g->s, buff)) ? free(buff) : 0;
